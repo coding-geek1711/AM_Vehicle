e25d425b842c4266814abae76eab5eadd5d878b7
Maheswaran Parameswaran <mahestpm@gmail.com>
2020-07-26 20:39:35 +0530

    Added Auto-Manual Check

--- ArduinoCode.ino
+++ ArduinoCode.ino
@@ -1,71 +1,82 @@
-/// Success ///
+/// Arduino Code with Manual and Auto Config ///
 
 #define a1 4
 #define a2 5
 #define b1 6
 #define b2 7
 
-int command;
+int command;      // Command to move
+int reciever;     // Data from NodeMCU
+int speed;        // Speed at which to run
+
+int DELAY = 30;
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  command = Serial.read();
-  Serial.println(command);
-  switch(command){
-    case 1:
-      Front();
-      break;
-    case 2:
-      Back();
-      break;
-    case 3:
-      Left();
-      break;
-    case 4:
-      Right();
-      break;
-    case 5:
-      Stop();
-      break;
-    default :
-      Stop();
-      break;
+  reciever = Serial.read();
+  Serial.println(reciever);
+  speed = reciever / 100;
+  command = reciever % 100;
+  while(reciever != 9999){
+      switch(command){
+      case 1:
+        Front(speed);
+        break;
+      case 2:
+        Back(speed);
+        break;
+      case 3:
+        Left(speed);
+        break;
+      case 4:
+        Right(speed);
+        break;
+      case 5:
+        Stop(speed);
+        break;
+      default :
+        Stop(speed);
+        break;
+    }
   }
-  delay(30);
+  if(reciever == 9999){
+    printf("Auto Code Here....");
+  }
+  delay(DELAY);
 }
 
-void Front(){
+void Front(int speed){
   digitalWrite(a1, HIGH);
   digitalWrite(a2, LOW);
   digitalWrite(b1, HIGH);
   digitalWrite(b2, LOW);
 }
 
-void Back(){
+void Back(int speed){
   digitalWrite(a1, LOW);
   digitalWrite(a2, HIGH);
   digitalWrite(b1, LOW);
   digitalWrite(b2, HIGH);
 }
 
-void Left(){
+void Left(int speed){
   digitalWrite(a1, LOW);
   digitalWrite(a2, LOW);
   digitalWrite(b1, HIGH);
   digitalWrite(b2, LOW);
 }
 
-void Right(){  
+void Right(int speed){  
   digitalWrite(a1, HIGH);
   digitalWrite(a2, LOW);
   digitalWrite(b1, LOW);
   digitalWrite(b2, LOW);
 }
 
-void Stop(){
+void Stop(int speed){
   digitalWrite(a1, HIGH);
   digitalWrite(a2, HIGH);
   digitalWrite(b1, HIGH);
